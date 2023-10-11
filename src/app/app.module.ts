4c0b1e45185e043c41f628aeef2e36be6703e555
Marcin <marcinegzmont@gmail.com>
2023-10-11 17:40:00 +0100

    slider styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FilmListComponent } from './pages/film-list/film-list.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, FilmListComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, FilmListComponent, SliderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
