241a1aed1580393b8d0c34955a71b24728d2d961
Marcin <marcinegzmont@gmail.com>
2023-10-12 09:30:06 +0100

    35 Displaying fetched data

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -7,12 +7,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
+  films: any;
+
   constructor(private http: HttpClient) {}
   ngOnInit() {
+    this.getpopularFilms();
+  }
+  getpopularFilms() {
     this.http
       .get(
         'https://api.themoviedb.org/3/movie/popular?api_key=92e327ad16fcc64c71ad3e66dbdabbc5'
       )
-      .subscribe((data) => console.log(data));
+      .subscribe((data) => (this.films = data));
   }
 }
