5da3361633cb313cbba4860ed96a8a097085c2dc
Marcin <marcinegzmont@gmail.com>
2023-10-14 14:37:49 +0100

    44 slide change

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -6,7 +6,13 @@ import { FilmsService } from 'src/app/sevices/films.service';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   constructor(private filmsService: FilmsService) {}
   films$ = this.filmsService.getPopularFilms();
+  slideIndex = 0;
+  ngOnInit() {
+    setInterval(() => {
+      this.slideIndex += 1;
+    }, 5000);
+  }
 }
