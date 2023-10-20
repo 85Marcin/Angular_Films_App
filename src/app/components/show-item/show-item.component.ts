cf517206dff4993da4c039698cc6708a0d5994d4
Marcin <marcinegzmont@gmail.com>
2023-10-20 21:44:52 +0100

    @Input

--- src/app/components/show-item/show-item.component.ts
+++ src/app/components/show-item/show-item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { imagesBaseUrl } from './../../constants/images-sizes';
+import { Component, Input } from '@angular/core';
+import { Film } from 'src/app/types/film';
 
 @Component({
   selector: 'app-show-item',
   templateUrl: './show-item.component.html',
-  styleUrls: ['./show-item.component.scss']
+  styleUrls: ['./show-item.component.scss'],
 })
 export class ShowItemComponent {
-
+  imageBaseUrl = imagesBaseUrl;
+  @Input() showItem: Film | null = null;
 }
