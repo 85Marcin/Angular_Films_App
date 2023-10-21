0996ac43833a8598464e9b69ca7cf5e39c5bc802
Marcin <marcinegzmont@gmail.com>
2023-10-21 19:46:18 +0100

    61 ID params

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
-  styleUrls: ['./show-detail.component.scss']
+  styleUrls: ['./show-detail.component.scss'],
 })
-export class ShowDetailComponent {
+export class ShowDetailComponent implements OnInit {
+  showID = 0;
+  constructor(private router: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.router.params.subscribe((params) => {
+      this.showID = params['id'];
+    });
+  }
 }
